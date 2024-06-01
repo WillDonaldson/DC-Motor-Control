f8a0942d101370ffce9a6c10dde90f4bc6a7a69f
WillDonaldson <accounts@willdonaldson.io>
2024-06-01 14:46:52 -0700

    created motor controller error codes

--- src/MotorDriver.cpp
+++ src/MotorDriver.cpp
@@ -2,14 +2,19 @@
 #include <Arduino.h>
 
 // Set the motor speed using PWM values
+// Does not return an error code. Errors and warnings are handled internally.
 void MotorDriver::setSpeedPWM(int speed){
-    speed = constrain(speed, -maxPWM, maxPWM);  // ensure speed is within valid range
-    speed = abs(speed) > minPWM ? speed : 0;    // apply minimum PWM threshold
+    if(abs(speed) < minPWM || abs(speed) > maxPWM){
+        handleError(WARNING_INVALID_PWM_SETPOINT);
+        speed = constrain(speed, -maxPWM, maxPWM);  // ensure speed is within valid range
+        speed = abs(speed) > minPWM ? speed : 0;    // apply minimum PWM threshold
+    }
     updateSpeedPWM(speed);                      // write filtered speed command to motors
     currentSpeedPWM = speed;                    // log current speed, for other functions to reference
 }
 
 // Stop the motor immediately by setting speed to 0
+// Does not return an error code. Errors and warnings are handled internally.
 void MotorDriver::stop(){
     setSpeedPWM(0);
 }
@@ -19,19 +24,57 @@ int MotorDriver::getPWM(){
 }
 
 // Set the minimum PWM value
-bool MotorDriver::setMinPWM(int PWM){
+// Returns error code or success
+MotorDriverError MotorDriver::setMinPWM(int PWM){
     if(PWM < 0 || PWM >= maxPWM){
-        return false;       // invalid PWM value
+        return handleError(ERROR_INVALID_PWM_CUTOFF);      
     }
     minPWM = PWM;
-    return true;
+    if(getPWM() < minPWM){
+        return handleError(WARNING_CURRENT_PWM_SPEED_INVALID); // after update, the current PWM speed is now invalid
+    }
+    return handleError(SUCCESS);
 }
 
 // Set the maximum PWM value
-bool MotorDriver::setMaxPWM(int PWM){
+// Returns error code or success
+MotorDriverError MotorDriver::setMaxPWM(int PWM){
     if(PWM > 255 || PWM <= minPWM){
-        return false;       // invalid PWM value
+        return handleError(ERROR_INVALID_PWM_CUTOFF);   
     }
     maxPWM = PWM;
-    return true;
-} 
+    if(getPWM() > maxPWM){
+        return handleError(WARNING_CURRENT_PWM_SPEED_INVALID); // after update, the current PWM speed is now invalid
+    }
+    return handleError(SUCCESS);
+}
+
+MotorDriverError MotorDriver::handleError(MotorDriverError error){
+    lastError = error;
+    switch(error){
+        case SUCCESS:
+            // TO DO: Handle success
+            break;
+        case ERROR_INVALID_PWM_CUTOFF:
+            // TO DO: Handle error of invalid request to update minPWM or maxPWM 
+            break;
+        case WARNING_INVALID_PWM_SETPOINT:
+            // TO DO: Handle warning of invalid requested PWM setpoint beyond maxPWM 
+            break;
+        case WARNING_CURRENT_PWM_SPEED_INVALID:
+            // Handle warning that current PWM speed is out of allowed range
+            if(currentSpeedPWM < minPWM){
+                // stop motor
+                setSpeedPWM(0);
+            }
+            else if(currentSpeedPWM > maxPWM){
+                // restrict motor to maxPWM
+                setSpeedPWM(maxPWM);
+            }
+            break;
+        default:
+            // TO DO: Handle unknown error
+            break;
+    }
+    return error;
+}
--- src/MotorDriver.h
+++ src/MotorDriver.h
@@ -1,22 +1,31 @@
 #ifndef MotorDriver_h
 #define MotorDriver_h
 
+enum MotorDriverError{
+    SUCCESS = 0,
+    ERROR_INVALID_PWM_CUTOFF = 1,
+    WARNING_INVALID_PWM_SETPOINT = 2,
+    WARNING_CURRENT_PWM_SPEED_INVALID = 3
+};
+
 // Base class for motor drivers
 class MotorDriver {
     public:
-        virtual ~MotorDriver() {}       // virtual destructor for cleanup
-        virtual void init() = 0;        // virtual function to initialize the motor driver
+        virtual ~MotorDriver() {}                   // virtual destructor for cleanup
+        virtual void init() = 0;                    // virtual function to initialize the motor driver
         void setSpeedPWM(int speed);    // non-virtual interface to set the motor speed using PWM values
         void stop();
+        MotorDriverError setMinPWM(int PWM);
+        MotorDriverError setMaxPWM(int PWM);
         int getPWM();
-        bool setMinPWM(int PWM);
-        bool setMaxPWM(int PWM);
     protected:
         virtual void updateSpeedPWM(int speed) = 0; // protected virtual function to be implemented by derived classes
-        int currentSpeedPWM = 0;        // variable updated each time setSpeedPWM is called
+        int currentSpeedPWM = 0;                    // variable updated each time setSpeedPWM is called
+        MotorDriverError handleError(MotorDriverError error);
     private:
         int minPWM = 0;         
         int maxPWM = 255;
+        MotorDriverError lastError = SUCCESS;      // for logging last error
 };
 
 #endif
